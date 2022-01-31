26eb5444258cde445f4e5d27731ee4cba70b37ed
Евгений Александров <alekevge@yandex.ru>
2022-01-31 20:45:48 +0300

    task 1-4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,17 @@
 function checkSpam(str) {
   // ваш код...
+  let spam1 = "1xBet"
+  let spam2 = "XXX"
+  str = toLowerCase(str)
+  return str.includes(toLowerCase(spam1)) ||  str.includes(toLowerCase(spam2))
+}
+
+function toLowerCase(str)
+{
+  //приведем все к одному регистру
+  let res=""
+  for (let i = 0; i < str.length; i++) {
+    res+=str[i].toLowerCase()
+  }
+  return res
 }
