e8e81352035c2b782ed52170ab9825fb289a3d3c
Евгений Александров <alekevge@yandex.ru>
2022-02-05 22:10:57 +0300

    3 module  tasks - 1,2,4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,19 @@
+function showSalary1(users, age) {
+  // ваш код...
+  let res=users.filter(x=>x.age<=age)
+               .map(x=>[x.name, x.balance].join(", "))
+               .join("\n")
+  return res
+}
+
+
 function showSalary(users, age) {
   // ваш код...
+  function reducer(sum, cur)
+  {
+    return (sum? sum+"\n" :sum) + (cur.age<=age?cur.name+", "+cur.balance:"")
+  }
+  return users.reduce((sum,cur) => cur.age <= age ?  reducer(sum,cur) : sum ,"")
 }
+
+//обе функции рабочие
