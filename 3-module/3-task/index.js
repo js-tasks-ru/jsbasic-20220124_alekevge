fd07ea4acf1c2064ba3840200ddae4eaaaa72a5c
Евгений Александров <alekevge@yandex.ru>
2022-02-07 21:34:27 +0300

    module-3 tasks 3,5

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,26 @@
 function camelize(str) {
   // ваш код...
+  let words = str.split("-")
+  function summator(res, word,index)
+  {
+    if (!word) return res
+    res += index?word[0].toUpperCase():word[0] 
+    return res+word.slice(1)
+  }
+
+  return  words.reduce(summator)
 }
+
+
+ function camelize1(str) {
+  // ваш код...
+  let words = str.split("-")
+  let res=""
+
+  for (let i = 0; i < words.length; i++) {
+    const word=words[i]
+    if (!i&& word) res+=word 
+    else if (word) res+=word[0].toUpperCase()+word.slice(1)
+  }
+  return  res
+} 
