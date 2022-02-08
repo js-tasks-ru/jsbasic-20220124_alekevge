09f1692f98f6ec05e5f869e165afb67ab8f3de7a
Евгений Александров <alekevge@yandex.ru>
2022-02-08 21:46:25 +0300

    4 - module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
   // ваш код...
+  for (let i=0; i<table.rows.length;i++)
+  {
+    const row = table.rows[i]
+    row.hidden = ![...row.cells].some((cell)=>cell.dataset.available!==undefined)
+
+    if (row.cells[3].dataset.available=="true") 
+      row.classList.add("available")
+    else
+      row.classList.add("unavailable")
+
+
+    if (row.cells[2].textContent=="f")
+      row.classList.add("female")
+    else
+      row.classList.add("male")
+
+
+    if (+row.cells[1].textContent<18) 
+    row.style.textDecoration= "line-through"
+
+  }
 }
