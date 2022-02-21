b44a8ff737ed40b7f664198a7261b41eedfa4346
Евгений Александров <alekevge@yandex.ru>
2022-02-22 01:34:50 +0300

    module 6

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,28 @@
 export default class ProductCard {
   constructor(product) {
+  this.elem=document.createElement("div")
+  this.elem.classList.add("card")
+  product.price=product.price.toFixed(2)
+  this.elem.innerHTML=`
+  <div class="card__top">
+      <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+      <span class="card__price">€${product.price}</span>
+  </div>
+  <div class="card__body">
+      <div class="card__title">${product.name}</div>
+      <button type="button" class="card__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+      </button>
+  </div> `
+
+   let btn=this.elem.getElementsByTagName("button")[0]
+    btn.onclick=()=>{
+        const readEvent = new CustomEvent('product-add', {
+            detail: product.id, 
+            bubbles: true 
+        });
+
+       this.elem.dispatchEvent(readEvent); 
+    }
   }
 }
